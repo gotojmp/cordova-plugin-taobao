1c326e4079b4b12db5e95ef7beeef03eb30f5eb5
gotojmp <gotojmp@gmail.com>
2015-12-01 18:03:24 +0800

    dev

---
+++ src/ios/sdk/ALBBTBOpenPlugin.framework/Headers/ALBBTBOpenPluginPluginServiceProtocol.h
@@ -0,0 +1,15 @@
+//
+//  ALBBTBOpenPluginPluginServiceProtocol.h
+//  ALBBTBOpenPluginPluginAdapter
+//
+//  Created by 友和(lai.zhoul@alibaba-inc.com) on 14/11/26.
+//  Copyright (c) 2014年 Alipay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ALBBTBOpenPluginPluginServiceProtocol <NSObject>
+
+-(BOOL) jumpTBDetail:(NSDictionary *) params ;
+
+@end
