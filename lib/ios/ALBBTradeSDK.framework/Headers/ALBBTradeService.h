150dfefcdc1f3dc326f3d6b849eb7def7c3877a9
gotojmp <gotojmp@gmail.com>
2015-12-01 18:26:03 +0800

    move sdk to lib

---
+++ lib/ios/ALBBTradeSDK.framework/Headers/ALBBTradeService.h
@@ -0,0 +1,38 @@
+//
+//  ALBBTradeService.h
+//  ALBBTradeSDK
+//  电商接口基础service
+//  Created by 友和(lai.zhoul@alibaba-inc.com) on 15/3/4.
+//  Copyright (c) 2015年 Alipay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "TaeTradeProcessResult.h"
+#import "TaeTaokeParams.h"
+#import <TaeSDK/TaeSDK.h> 
+
+@protocol ALBBTradeService <NSObject>
+
+/**
+ * 交易流程结果回调
+ */
+typedef void (^tradeProcessSuccessCallback)(TaeTradeProcessResult *tradeProcessResult);
+typedef void (^tradeProcessFailedCallback)(NSError *error);
+
+
+/**
+ *  加入商品到购物车的结果回调
+ */
+typedef void (^addCartSuccessCallback)();
+typedef void (^addCartCacelledCallback)();
+
+
+@end
+
+
+@interface TaeSDK(TaoKe)
+
+-(void) setTaoKeParams:(TaeTaokeParams *) params;
+
+@end
